a0d338ca66fecbef0d7f128ce5fba36a38bd755f
Jokez <bissonjoshua@hotmail.fr>
2024-10-09 13:42:54 +0200

    UI

--- DuelmieuxqueNestor/Assets/_AssetsJeux/Taupes/Scripts/StartRound.cs
+++ DuelmieuxqueNestor/Assets/_AssetsJeux/Taupes/Scripts/StartRound.cs
@@ -7,10 +7,16 @@ using UnityEngine.UI;
 public class StartRound : MonoBehaviour
 {
     public Text StartText;
+    public Text FirsText;
 
     // Start is called before the first frame update
     IEnumerator Start()
     {
+        StartText.text = " ";
+        FirsText.text = ("Le premier à 5 gagne!");
+        yield return new WaitForSeconds(1);
+        Destroy(FirsText.gameObject);
+
         for (int i = 3; i >0; i--)
         {
             StartText.text = i.ToString();
